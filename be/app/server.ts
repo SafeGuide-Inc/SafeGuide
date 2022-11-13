46cb1f8129a174d4c3e8030a0569c956c097534c
bauerbr <bryan@osmind.org>
2022-11-12 23:04:27 -0800

    docker + graphql working

--- be/app/server.ts
+++ be/app/server.ts
@@ -1,9 +1,71 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import {
+  Application,
+  Router,
+  Middleware,
+} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { GraphQLHTTP } from "https://deno.land/x/gql@1.1.2/mod.ts";
+import { makeExecutableSchema } from "https://deno.land/x/graphql_tools@0.0.2/mod.ts";
+import { gql } from "https://deno.land/x/graphql_tag@0.0.1/mod.ts";
 
-const app = new Application();
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+const resolvers = {
+  Query: {
+    hello: (_root: undefined, _args: unknown, ctx: { request: Request }) => {
+      return `Hello World! from ${ctx.request.url}`;
+    },
+  },
+};
+
+const schema = makeExecutableSchema({ resolvers, typeDefs });
+
+const resolve = GraphQLHTTP({
+  schema,
+  graphiql: true,
+  context: (request) => ({ request }),
+});
+
+const handleGraphQL: Middleware = async (ctx) => {
+  // cast Oak request into a normal Request
+  const req = new Request(ctx.request.url.toString(), {
+    body: ctx.request.originalRequest.getBody().body,
+    headers: ctx.request.headers,
+    method: ctx.request.method,
+  });
+
+  const res = await resolve(req);
+
+  for (const [k, v] of res.headers.entries()) ctx.response.headers.append(k, v);
+
+  ctx.response.status = res.status;
+  ctx.response.body = res.body;
+};
+
+// Allow CORS:
+// const cors: Middleware = (ctx) => {
+// ctx.response.headers.append('access-control-allow-origin', '*')
+// ctx.response.headers.append('access-control-allow-headers', 'Origin, Host, Content-Type, Accept')
+// }
+
+const graphqlRouter = new Router().all("/graphql", handleGraphQL);
+
+const app = new Application().use(
+  // cors,
+  graphqlRouter.routes(),
+  graphqlRouter.allowedMethods()
+);
+
+app.addEventListener("listen", ({ secure, hostname, port }) => {
+  if (hostname === "0.0.0.0") hostname = "localhost";
+
+  const protocol = secure ? "https" : "http";
+  const url = `${protocol}://${hostname ?? "localhost"}:${port}`;
+
+  console.log("☁  Started on " + url);
 });
 
 await app.listen({ port: 3000 });
