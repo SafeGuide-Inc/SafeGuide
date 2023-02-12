72d62482f8e39d9971011a96d9420d9921b82c56
bauerbr <bryan@osmind.org>
2023-02-12 08:51:20 -0800

    fix

--- be/app/models/schema.ts
+++ be/app/models/schema.ts
@@ -1,11 +1,11 @@
 import { makeExecutableSchema } from "https://deno.land/x/graphql_tools@0.0.2/mod.ts";
+import { gql } from "https://deno.land/x/graphql_tag@0.0.1/mod.ts";
 
 const typeDefs = gql`
   type Query {
     hello: String
   } 
 `;
-
 const resolvers = {
   Query: {
     hello: (_root: undefined, _args: unknown, ctx: { request: Request }) => {
