2a3e34fa443a06f7b379b7ced0de0da20405ed01
bauerbr <bryan@osmind.org>
2023-06-07 17:07:42 -0700

    update types migration

--- be/app/prisma/migrations/20230326094757_add_incidence_types/migration.sql
+++ be/app/prisma/migrations/20230326094757_add_incidence_types/migration.sql
@@ -1,24 +1,122 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-INSERT INTO incidence_type(
-    id,name, description, category)
-	VALUES (uuid_generate_v4(),'Car Theft', '', 'Theft'),
-	(uuid_generate_v4(),'Home Break-in', '', 'Theft'),
-	(uuid_generate_v4(),'Pickpocket', '', 'Theft'),
-	(uuid_generate_v4(),'Stolen Item', '', 'Theft'),
-	(uuid_generate_v4(),'Fight', '', 'Violence'),
-	(uuid_generate_v4(),'Gun Pulled', '', 'Violence'),
-	(uuid_generate_v4(),'Knife Pulled', '', 'Violence'),
-	(uuid_generate_v4(),'Weaponized Object', '', 'Violence'),
-	(uuid_generate_v4(),'Stalking', '', 'ThreateningPerson'),
-	(uuid_generate_v4(),'Lingering', '', 'ThreateningPerson'),
-	(uuid_generate_v4(),'Harassment', '', 'ThreateningPerson'),
-	(uuid_generate_v4(),'Yelling', '', 'SuspiciousActivity'),
-	(uuid_generate_v4(),'Loud Noise', '', 'SuspiciousActivity'),
-	(uuid_generate_v4(),'Argument', '', 'SuspiciousActivity'),
-	(uuid_generate_v4(),'Fire', '', 'Hazard'),
-	(uuid_generate_v4(),'Powerline', '', 'Hazard'),	     
-	(uuid_generate_v4(),'Flood', '', 'Hazard'),	     
-	(uuid_generate_v4(),'Car Crash', '', 'Hazard'),	     
-	(uuid_generate_v4(),'General', '', 'GeneralIncident'),
-	(uuid_generate_v4(),'Emergency Responder', '', 'EmergencyResponder');	
+INSERT INTO incidence_type (id, name, description, category)
+VALUES
+    (
+        '123e4567-e89b-12d3-a456-426655440000',
+        'Car Theft',
+        'Incidence of a vehicle being stolen or attempted theft of a vehicle.',
+        'Theft'
+    ),
+    (
+        '223e4567-e89b-12d3-a456-426655440000',
+        'Home Break-in',
+        'Incidence where an unauthorized person breaks into a home with the intent to commit an offence such as theft.',
+        'Theft'
+    ),
+    (
+        '323e4567-e89b-12d3-a456-426655440000',
+        'Pickpocket',
+        'Incidence where an individuals belongings are stolen from their person without their awareness.',
+        'Theft'
+    ),
+    (
+        '423e4567-e89b-12d3-a456-426655440000',
+        'Stolen Item',
+        'Incidence of a personal belonging being taken without permission.',
+        'Theft'
+    ),
+    (
+        '523e4567-e89b-12d3-a456-426655440000',
+        'Fight',
+        'Incidence where two or more individuals engage in a physical conflict.',
+        'Violence'
+    ),
+    (
+        '623e4567-e89b-12d3-a456-426655440000',
+        'Gun Pulled',
+        'Incidence where an individual brandishes a firearm in a threatening manner.',
+        'Violence'
+    ),
+    (
+        '723e4567-e89b-12d3-a456-426655440000',
+        'Knife Pulled',
+        'Incidence where an individual brandishes a knife in a threatening manner.',
+        'Violence'
+    ),
+    (
+        '823e4567-e89b-12d3-a456-426655440000',
+        'Weaponized Object',
+        'Incidence where an object is used as a weapon in a threatening or violent manner.',
+        'Violence'
+    ),
+    (
+        '923e4567-e89b-12d3-a456-426655440000',
+        'Stalking',
+        'Incidence where an individual is being followed or observed in a persistent, intrusive manner.',
+        'ThreateningPerson'
+    ),
+    (
+        'a23e4567-e89b-12d3-a456-426655440000',
+        'Lingering',
+        'Incidence of a person remaining in a place longer than necessary, often in a way that seems suspicious or unnerving.',
+        'ThreateningPerson'
+    ),
+    (
+        'b23e4567-e89b-12d3-a456-426655440000',
+        'Harassment',
+        'Incidence of aggressive pressure or intimidation.',
+        'ThreateningPerson'
+    ),
+    (
+        'c23e4567-e89b-12d3-a456-426655440000',
+        'Yelling',
+        'Incidence of a person yelling in an aggressive or disruptive manner.',
+        'SuspiciousActivity'
+    ),
+    (
+        'd23e4567-e89b-12d3-a456-426655440000',
+        'Loud Noise',
+        'Incidence of a suspiciously loud noise or disturbance.',
+        'SuspiciousActivity'
+    ),
+    (
+        'e23e4567-e89b-12d3-a456-426655440000',
+        'Argument',
+        'Incidence of a heated disagreement that could potentially escalate to physical violence.',
+        'SuspiciousActivity'
+    ),
+    (
+        'f23e4567-e89b-12d3-a456-426655440000',
+        'Fire',
+        'Incidence of a fire breakout that poses a potential risk to lives or property.',
+        'Hazard'
+    ),
+    (
+        '023f4567-e89b-12d3-a456-426655440000',
+        'Powerline',
+        'Incidence related to a fallen or damaged powerline that may pose a risk.',
+        'Hazard'
+    ),
+    (
+        '123f4567-e89b-12d3-a456-426655440000',
+        'Flood',
+        'Incidence of flooding that might pose a threat to people or property.',
+        'Hazard'
+    ),
+    (
+        '223f4567-e89b-12d3-a456-426655440000',
+        'Car Crash',
+        'Incidence of a vehicular collision causing potential harm or damage.',
+        'Hazard'
+    ),
+    (
+        '323f4567-e89b-12d3-a456-426655440000',
+        'General',
+        'An incidence that does not fit into other defined categories but warrants attention or reporting.',
+        'GeneralIncident'
+    ),
+    (
+        '423f4567-e89b-12d3-a456-426655440000',
+        'Emergency Responder',
+        'Incidence where emergency responders like police, firefighters, or paramedics are present or required.',
+        'EmergencyResponder'
+    );
